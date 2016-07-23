db765efa4689dcd9f786753dee9d1211d1d0dab5
mikusher <mikusher@hotmail.com>
2016-07-23 01:38:26 +0100

    Adicionando codigo do SqL com modificações

--- src/cv/mikusher/qsllite/Sql6_SelectData.java
+++ src/cv/mikusher/qsllite/Sql6_SelectData.java
@@ -1,5 +1,11 @@
 /*
- * Copyright 2016 Miky Mikusher Wayne.
+ * Copyright (C) 2016 Miky Mikusher Wayne
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  */
 package cv.mikusher.qsllite;
 
