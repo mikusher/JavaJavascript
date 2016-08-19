5bd43398e7902b3acbb413d7686afe55a7f463c7
Mikusher <Mikusher@Mikusher-PC>
2016-08-19 23:34:56 +0100

    game

---
+++ src/cv/mikusher/outros/LerPaginaWeb.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 Mikusher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+package cv.mikusher.outros;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Mikusher
+ */
+public class LerPaginaWeb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        // TODO code application logic here
+        BufferedReader br = null;
+        
+        try {
+            URL url = new URL("http://www.gmail.com");
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+            
+            String line;
+            StringBuilder sb = new StringBuilder();
+            
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+            System.out.println(sb);
+        
+        } finally {
+            if (br != null){
+                br.close();
+            }        
+        }
+    }
+}
