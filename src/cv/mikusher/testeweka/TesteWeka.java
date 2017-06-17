f41eae67cc4b42a9017f0b227e0a1db24d288fdf
latavares <latavares@mikusher-pc>
2017-06-17 21:57:51 +0100

    comentario no codigo WEKA

--- src/cv/mikusher/testeweka/TesteWeka.java
+++ src/cv/mikusher/testeweka/TesteWeka.java
@@ -34,24 +34,35 @@ public class TesteWeka {
      *            the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        // TODO code application logic here
 
+        // criação do datasouce, indicando o caminho da BD "vendas"
         DataSource ds = new DataSource("src/cv/mikusher/testeweka/vendas.arff");
-        Instances ins = ds.getDataSet();
-        // System.out.println(ins.toString());
 
+        // pega as instancias da base de dados
+        Instances ins = ds.getDataSet();
+
+        // indicar a classe da que vai ser feito a previsão {3} "@attribute gasta_muito {Nao, Sim}"
         ins.setClassIndex(3);
 
+        // algoritmo em que vai ser utilizado na previsão
         NaiveBayes nb = new NaiveBayes();
         nb.buildClassifier(ins);
 
+        // criando o classificador - ele recebe o nimero de atributos que tem na BD {sexo, idade, filhos, gasta_muito}
         DenseInstance novo = new DenseInstance(4);
+
+        // associar o novo elemento da previsão - relacionar o novo com o a base de dados
         novo.setDataset(ins);
+
+        // os atributos do novo elemento -- o atributo da previsão não deve ser passada @attribute gasta_muito por ser este que queremos prever
         novo.setValue(0, "M");
         novo.setValue(1, "18-39");
         novo.setValue(2, "Sim");
 
+        // fazendo a previsão do elemento novo
         double probabilidade[] = nb.distributionForInstance(novo);
+
+        // imprimindo os resultados
         System.out.println("Não: " + probabilidade[0]);
         System.out.println("");
         System.out.println("Sim: " + probabilidade[1]);
