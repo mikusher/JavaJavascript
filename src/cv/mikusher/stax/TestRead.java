8101221d3686326bf51c25125ebc0a6dd8dfd48c
Luis Tavares <mikusher@hotmail.com>
2017-07-12 20:45:13 +0100

    new

---
+++ src/cv/mikusher/stax/TestRead.java
@@ -0,0 +1,28 @@
+
+
+
+
+
+package cv.mikusher.stax;
+
+
+
+
+
+import java.util.List;
+
+
+
+
+
+public class TestRead {
+
+    public static void main(String args[]) {
+
+        StaXParser read = new StaXParser();
+        List<Item> readConfig = read.readConfig("config.xml");
+        for (Item item : readConfig) {
+            System.out.println(item);
+        }
+    }
+}
