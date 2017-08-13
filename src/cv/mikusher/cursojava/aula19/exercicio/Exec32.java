f182a97f2736232882b7a7ace7bf8f62d4bbf273
Mikusher <Mikusher@Mikusher-PC>
2017-08-13 17:10:35 +0100

    new Exec

---
+++ src/cv/mikusher/cursojava/aula19/exercicio/Exec32.java
@@ -0,0 +1,23 @@
+package cv.mikusher.cursojava.aula19.exercicio;
+
+import java.util.Scanner;
+
+public class Exec32 {
+
+    public static void main(String[] args) {
+
+        int[] contA = new int[5];
+        Scanner scan = new Scanner(System.in);
+        
+        for (int i = 0; i < contA.length; i++) {
+            System.out.print("Indica um novo valor: ");
+            contA[i] = scan.nextInt();
+        }
+        
+        for (int j = 1; j <= 10; j++) {
+            for (int i = 0; i < contA.length; i++) {
+                System.out.println(contA[i]+" x " + j +" = " + (contA[i]*j));
+            }
+        }
+    }
+}
