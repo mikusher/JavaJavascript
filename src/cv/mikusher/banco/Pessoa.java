af77d2db806eaacff2a01a3ed776533d3f253470
Mikusher <Mikusher@Mikusher-PC>
2017-08-15 20:45:22 +0100

    Changes

---
+++ src/cv/mikusher/banco/Pessoa.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2017 Miky Mikusher
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
+
+
+
+package cv.mikusher.banco;
+
+
+
+
+
+import cv.mikusher.agenda.*;
+import java.io.Serializable;
+import java.util.Random;
+import java.util.UUID;
+
+
+
+
+
+/**
+ *
+ * @author Luis Amilcar D. N. Tavares
+ * @nickname Miky Mikusher
+ */
+
+public class Pessoa implements Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String            _nome, _endereco, _genero;
+    private int               _idade, _salario;
+    private transient int     _id;
+    private int               _idPesquisa;
+    private String            _randomUUIDString;
+
+
+
+
+
+    Pessoa() {}
+
+
+
+
+
+    Pessoa(int id, String nome, int idade, String genero) {
+
+        this._id = id;
+        this._nome = nome;
+        this._idade = idade;
+        this._genero = genero;
+
+    }
+
+
+
+
+
+    public String setUUID(UUID uuid_) {
+
+        this._randomUUIDString = UUID.randomUUID()
+                                     .toString();
+        _randomUUIDString.replaceAll("-", "");
+
+        return _randomUUIDString;
+    }
+
+
+
+
+
+    public String getUUID() {
+
+        return _randomUUIDString;
+    }
+
+
+
+
+
+    Pessoa(String nome, int idade, String genero) {
+
+        this._nome = nome;
+        this._idade = idade;
+        this._genero = genero;
+
+    }
+
+
+
+
+
+    public String toString() {
+
+        return "Nome= " + _nome + "\nGenero= " + _genero + "\nIdade= " + _idade;
+    }
+
+
+
+
+
+    public String getNome() {
+
+        return _nome;
+    }
+
+
+
+
+
+    public void setNome(String nome) {
+
+        this._nome = nome;
+    }
+
+
+
+
+
+    public int getIdade() {
+
+        return _idade;
+    }
+
+
+
+
+
+    public void setIdade(int idade) {
+
+        this._idade = idade;
+    }
+
+
+
+
+
+    public String getEndereco() {
+
+        return _endereco;
+    }
+
+
+
+
+
+    public void setEndereco(String endereco) {
+
+        this._endereco = endereco;
+    }
+
+
+
+
+
+    public int getId() {
+
+        return _id;
+    }
+
+
+
+
+
+    public void setId(int id) {
+
+        this._id = id;
+    }
+
+
+
+
+
+    public int getidPesquisa() {
+
+        return _idPesquisa;
+    }
+
+
+
+
+
+    public void setidPesquisa(int _idPesquisa) {
+
+        this._idPesquisa = _idPesquisa;
+    }
+
+
+
+
+    public void setSalario(int _salario) {
+        this._salario = _salario;
+    }
+
+    
+    public int getSalario() {
+        Random rnd = new Random();
+        _salario = rnd.nextInt(10000 - 1000 + 1) + 1000;
+        return _salario;
+    }
+    
+}
---
+++ src/cv/mikusher/banco/WriterReader.java
@@ -0,0 +1,74 @@
+
+
+
+
+
+package cv.mikusher.banco;
+
+
+
+
+
+import cv.mikusher.banco.Pessoa;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+
+
+
+public class WriterReader {
+
+    static String caminho = "src/cv/mikusher/banco/dados/myObjects.txt";
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa("Cintia", 31, "Feminino");
+        Pessoa p2 = new Pessoa("Luis", 29, "Masculino");
+
+        try {
+            FileOutputStream f = new FileOutputStream(new File(caminho));
+            ObjectOutputStream o = new ObjectOutputStream(f);
+
+            // escrever para o ficheiro
+            o.writeObject(p1);
+            o.writeObject(p2);
+
+            o.close();
+            f.close();
+
+            FileInputStream fi = new FileInputStream(new File(caminho));
+            ObjectInputStream oi = new ObjectInputStream(fi);
+
+            // ler objectos do ficheiro
+            Pessoa pessoa_1 = (Pessoa) oi.readObject();
+            Pessoa pessoa_2 = (Pessoa) oi.readObject();
+
+            System.out.println(pessoa_1.toString());
+            System.out.println();
+            System.out.println(pessoa_2.toString());
+
+            oi.close();
+            fi.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error initializing stream");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
