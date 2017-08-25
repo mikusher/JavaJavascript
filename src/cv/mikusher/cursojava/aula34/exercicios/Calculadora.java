dd51106989bc0600e000ff499910a33b10a348e8
Mikusher <Mikusher@Mikusher-PC>
2017-08-25 20:10:25 +0100

    togo

--- src/cv/mikusher/cursojava/aula34/exercicios/Calculadora.java
+++ src/cv/mikusher/cursojava/aula34/exercicios/Calculadora.java
@@ -62,4 +62,22 @@ public class Calculadora {
         }
     }
 
+
+
+
+
+    public static int fatorial(int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int total = 1;
+        for (int i = num; i > 1; i--) {
+            total *= i;
+        }
+
+        return total;
+    }
+
 }
