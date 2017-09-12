8657efccf9eeb52f77e97813b266a50b21cbeb9a
Luis Amilcar Tavares <mikusher@hotmail.com>
2017-09-12 21:54:13 +0100

    para aula 53

---
+++ src/cv/mikusher/cursojava/aula53/Teste.java
@@ -0,0 +1,79 @@
+
+
+
+
+
+package cv.mikusher.cursojava.aula53;
+
+
+
+
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+        usandoConstantes();
+
+    }
+
+
+
+
+
+    private static void usandoConstantes() {
+
+        int segunda = DiaSemanaConstantes.SEGUNDA;
+        int terca = DiaSemanaConstantes.TERCA;
+        int quarta = DiaSemanaConstantes.QUARTA;
+        int quinta = DiaSemanaConstantes.QUINTA;
+        int sexta = DiaSemanaConstantes.SEXTA;
+        int sabado = DiaSemanaConstantes.SABADO;
+        int domingo = DiaSemanaConstantes.DOMINGO;
+
+        System.out.println("Utilizando constantes: ");
+        imprimirDiaSemana(segunda);
+        imprimirDiaSemana(terca);
+        imprimirDiaSemana(quarta);
+        imprimirDiaSemana(quinta);
+        imprimirDiaSemana(sexta);
+        imprimirDiaSemana(sabado);
+        imprimirDiaSemana(domingo);
+    }
+
+
+
+
+
+    private static void imprimirDiaSemana(int dia) {
+
+        switch (dia) {
+            case 1:
+                System.out.println("Segunda-Feira");
+                break;
+            case 2:
+                System.out.println("Terça-Feira");
+                break;
+            case 3:
+                System.out.println("Quarta-Feira");
+                break;
+            case 4:
+                System.out.println("Quinta-Feira");
+                break;
+            case 5:
+                System.out.println("Sexta-Feira");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Dia não encontrado");
+                break;
+        }
+    }
+
+}
