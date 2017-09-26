67c4a9ff3c7c60dbdeff32271cd6782adfb68866
Luis Amilcar Tavares <mikusher@hotmail.com>
2017-09-26 23:28:45 +0100

    Enum

--- src/cv/mikusher/cursojava/aula75/Aula80.java
+++ src/cv/mikusher/cursojava/aula75/Aula80.java
@@ -19,6 +19,17 @@ public class Aula80 {
         System.out.println(teste.substring(10));
         System.out.println(teste.substring(10, 13));
 
+        String espacos = "i s p a ç o";
+        String semEspaco = espacos.replace('i', 'E');
+        System.out.println(semEspaco.toLowerCase());
+
+        semEspaco = semEspaco.replaceAll(" ", "");
+        System.out.println(semEspaco);
+
+        String nome = " Miky Mikusher ";
+        System.out.println(nome);
+        System.out.println(nome.trim().toUpperCase());
+
     }
 
 }
---
+++ src/cv/mikusher/cursojava/aula75/Aula82.java
@@ -0,0 +1,14 @@
+package cv.mikusher.cursojava.aula75;
+
+public class Aula82 {
+    public static void main(String[] args) {
+        String alfabeto = String.join(",", "A","B","C","D");
+
+        System.out.println(alfabeto);
+
+        String[] letras = alfabeto.split(", ");
+        for (String letra : letras) {
+            System.out.println(letra);
+        }
+    }
+}
