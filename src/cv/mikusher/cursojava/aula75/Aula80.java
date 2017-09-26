c0c7f60ac59f116c027b1b5f3c239ef8ab8db39f
Luis Amilcar Tavares <mikusher@hotmail.com>
2017-09-26 23:05:41 +0100

    str

---
+++ src/cv/mikusher/cursojava/aula75/Aula80.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+package cv.mikusher.cursojava.aula75;
+
+
+
+
+
+public class Aula80 {
+
+    public static void main(String[] args) {
+
+        String teste = "Teste de String";
+        System.out.println(teste);
+
+        System.out.println(teste.substring(10));
+        System.out.println(teste.substring(10, 13));
+
+    }
+
+}
