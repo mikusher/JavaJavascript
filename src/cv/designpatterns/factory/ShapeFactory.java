dc2fc84ad13b3800420541a34d584306ac7aeb63
Miky Mikusher <mikusher@hotmail.com>
2019-01-18 22:52:39 +0000

    Design Pattern

---
+++ src/cv/designpatterns/factory/ShapeFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2019 Miky Mikusher
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package cv.designpatterns.factory;
+
+
+/**
+ * @author Miky Mikusher
+ *
+ */
+public class ShapeFactory {
+
+    public Shape getShape(String shapeType) {
+
+
+        return null;
+    }
+
+}
---
+++ src/cv/designpatterns/factory/Square.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2019 Miky Mikusher
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+package cv.designpatterns.factory;
+
+
+/**
+ * @author Miky Mikusher
+ *
+ */
+public class Square implements Shape {
+
+    /*
+     * (non-Javadoc)
+     * @see cv.designpatterns.factory.Shape#draw()
+     */
+    @Override
+    public void draw() {
+
+        System.out.println("Inside Square::draw() method");
+
+    }
+
+}
