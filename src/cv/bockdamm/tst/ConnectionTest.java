c210681c09986e04c48253d65e4ca58238613fca
Luis Tavares <mikusher@hotmail.com>
2019-04-01 02:36:26 +0100

    Control

--- src/cv/bockdamm/tst/ConnectionTest.java
+++ src/cv/bockdamm/tst/ConnectionTest.java
@@ -1,9 +1,5 @@
 package cv.bockdamm.tst;
 
-import cv.bockdamm.connsecurity.Connectionsql;
-import weka.core.Instances;
-import weka.experiment.InstanceQuery;
-
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -11,16 +7,18 @@ import java.sql.DriverManager;
 public class ConnectionTest {
 
     public static void main(String[] args) throws Exception {
-        String GENERAL_TABLE      = "basecontroller";
-        String postgresconnection = "jdbc:postgresql://localhost:5432/" + GENERAL_TABLE;
-        String postgresuser       = "miky";
-        String postgrespassword   = "mikusher";
+		String DatabaseName = "basecontroller";
+		String postgresconnection = "jdbc:postgresql://localhost:5432/" + DatabaseName;
+		String postgresusr = "miky";
+		String postgrespsw = "mikusher";
 
-        Class.forName("org.postgresql.Driver");
-        Connection dbpsql = DriverManager.getConnection(postgresconnection, postgresuser, postgrespassword);
-        DatabaseMetaData dbpsqlmd = dbpsql.getMetaData();
-        System.out.println("Connection to " + dbpsqlmd.getDatabaseProductName() + " " + dbpsqlmd.getDatabaseProductVersion() + " successful.\n");
-        dbpsql.close();
+		Class.forName("org.postgresql.Driver");
 
+		try (Connection dbpsql = DriverManager.getConnection(postgresconnection, postgresusr, postgrespsw)) {
+			DatabaseMetaData dbpsqlmd = dbpsql.getMetaData();
+
+			System.out.println("Connection to " + dbpsqlmd.getDatabaseProductName() + " "
+					+ dbpsqlmd.getDatabaseProductVersion() + " successful.\n");
+		}
     }
 }
