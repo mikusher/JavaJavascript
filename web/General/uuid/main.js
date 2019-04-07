37ed15c94561db40eeaeeb0584ad50ba5fc067f1
Luis Tavares <mikusher@hotmail.com>
2019-04-07 12:43:09 +0100

    changes uuid

--- web/General/uuid/main.js
+++ web/General/uuid/main.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
 
     var UUIDPosition = document.getElementById('uuid_p');
     var btnGenerateNewUUID = document.getElementById('btnGenerateNewUUID');
+    var btnCopyUUID = document.getElementById('btnCopyUUID');
 
+    //initial presentation
     UUIDPosition.innerHTML = uuid();
 
+    //button generate new uuid
     btnGenerateNewUUID.addEventListener("click", function () {
         UUIDPosition.innerHTML = uuid();
+    });
+
+    //button copy uuid
+    btnCopyUUID.addEventListener("click", function () {
+        copyToClipboard();
     });
 
     //generate new uuid
@@ -16,7 +24,7 @@ $(document).ready(function () {
             seed += performance.now();
         }
 
-        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var uuid = 'xxxxxxxx-xxxx-xxxx-yxxx-wxxexxdxxoxx'.replace(/[xy]/g, function (c) {
             var r = (seed + Math.random() * 16) % 16 | 0;
             seed = Math.floor(seed/16);
 
@@ -24,6 +32,27 @@ $(document).ready(function () {
         });
 
         return uuid;
-    }
+    };
+
+    //copy uuid
+    function copyToClipboard () {
+        var el = document.createElement('textarea');  // Create a <textarea> element
+        el.value = $(document.getElementById('uuid_p')).text();  // Set its value to the string that you want copied
+        el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+        document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+        var selected =
+            document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+                ? document.getSelection().getRangeAt(0)     // Store selection if found
+                : false;                                    // Mark as false to know no selection existed before
+        el.select();                                    // Select the <textarea> content
+        document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+        document.body.removeChild(el);                  // Remove the <textarea> element
+        if (selected) {                                 // If a selection existed before copying
+            document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+            document.getSelection().addRange(selected);   // Restore the original selection
+        }
+    };
 
 });
