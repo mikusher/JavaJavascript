5fdfc87fcad4b52e56e02381725cc48fb6bf6c9d
Luis Tavares <mikusher@hotmail.com>
2019-04-23 21:02:47 +0100

    Connection configuration

--- src/cv/bockdamm/tst/ConnectionTest.java
+++ src/cv/bockdamm/tst/ConnectionTest.java
@@ -5,11 +5,9 @@ package cv.bockdamm.tst;
 
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 
@@ -18,27 +16,24 @@ import java.util.Properties;
 
 public class ConnectionTest {
 
-	public String makeConnection() throws IOException, ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
+
         Properties props = new Properties();
-		FileInputStream fis = new FileInputStream("src/com/mikusher/dwr/connection/db.properties");
-        props.load(fis);
-        fis.close();
-        String connectionShow;
+		FileInputStream fis = new FileInputStream("src/cv/bockdamm/tst/db.properties");
+		props.load(fis);
+		fis.close();
 
-        String postgresconnection = props.getProperty("postgres.url") + props.getProperty("postgres.dbName");
-        String postgresusr = props.getProperty("postgres.user");
-        String postgrespsw = props.getProperty("postgres.passwd");
+		String postgresconnection = props.getProperty("postgres.url") + props.getProperty("postgres.dbName");
+		String postgresusr = props.getProperty("postgres.user");
+		String postgrespsw = props.getProperty("postgres.passwd");
 
-        Class.forName(props.getProperty("postgres.driver"));
+		Class.forName(props.getProperty("postgres.driver"));
 
 		try (Connection dbpsql = DriverManager.getConnection(postgresconnection, postgresusr, postgrespsw)) {
 			DatabaseMetaData dbpsqlmd = dbpsql.getMetaData();
 
-			connectionShow = "Connection to " + dbpsqlmd.getDatabaseProductName() + " "
-					+ dbpsqlmd.getDatabaseProductVersion() + " successful.\n";
+			System.out.println("Connection to " + dbpsqlmd.getDatabaseProductName() + " "
+					+ dbpsqlmd.getDatabaseProductVersion() + " successful.\n");
 		}
-		
-		return connectionShow;
-
 	}
 }
