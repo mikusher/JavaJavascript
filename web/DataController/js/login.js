1c5d5caed2b2e6ccbb68f77a1e9011d8c6b1e606
Luis Tavares <mikusher@hotmail.com>
2019-05-23 19:50:08 +0100

    Login controller in page white dashboard

---
+++ web/DataController/js/login.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    const inputBTSignInd = document.getElementById('inputBTSignInd');
+    inputBTSignInd.addEventListener('click', login);
+
+    //login controller
+    function login() {
+        const inputEmail = document.getElementById('inputEmail');
+        const inputPassword = document.getElementById('inputPassword');
+
+        if (inputEmail.value === "" || inputPassword.value === "") {
+            document.getElementById('error').innerHTML = "<center class='text-warning'>Please Complete The Required Field!</center>";
+        } else {
+            if (inputEmail.value === 'luis@amilcar.cv' && inputPassword.value === 'tavares') {
+                document.getElementById('error').innerHTML = "";
+                inputEmail.value = "";
+                inputPassword.value = "";
+                window.location = "../pages/dashboard.html";
+            } else {
+                document.getElementById('error').innerHTML = "<center class='text-danger'>Invalid Username or Password</center>";
+            }
+        }
+    }
+
+    function toggleResetPswd(e){
+        e.preventDefault();
+        $('#logreg-forms .form-signin').toggle(); // display:block or none
+        $('#logreg-forms .form-reset').toggle(); // display:block or none
+    }
+
+    function toggleSignUp(e){
+        e.preventDefault();
+        $('#logreg-forms .form-signin').toggle(); // display:block or none
+        $('#logreg-forms .form-signup').toggle(); // display:block or none
+    }
+
+    $(()=>{
+        // Login Register Form
+        $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+        $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+        $('#logreg-forms #btn-signup').click(toggleSignUp);
+        $('#logreg-forms #cancel_signup').click(toggleSignUp);
+    })
+
+});
